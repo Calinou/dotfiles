fd936ddf664f5a5085e9453a3e7dc831a2692540
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-07-13 10:15:27 +0200

    Update package lists to install in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,16 +8,16 @@ COLOR_GREEN="\e[1;32m"
 COLOR_RESET="\e[0m"
 
 cargo install \
-		bat cargo-bloat clippy cross exa fd-find hyperfine oxipng ripgrep rls \
-		rustfmt tealdeer tokei &
+		bat cargo-bloat cargo-update clippy cross exa fd-find hyperfine oxipng \
+		ripgrep rls rustfmt tealdeer tokei watchexec &
 
 pip3 install --user --upgrade \
 		pip setuptools httpie youtube-dl pipenv speedtest-cli &
 
 yarn global add \
-		clean-css-cli colour-cli gulp-cli lighthouse n npm-check-updates npms-cli \
-		npx ntl prettier randomatic-cli serve stylelint tslint typescript uncss \
-		wappalyzer
+		clean-css-cli colour-cli eslint gulp-cli lighthouse n npm-check-updates \
+		npms-cli npx ntl prettier randomatic-cli serve stylelint tslint typescript \
+		uncss wappalyzer
 
 echo -e "\n$COLOR_GRAY"
 printf '—%.0s' {1..80}
