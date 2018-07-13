446d505f12045268469d9f7b30841bd93cfa3dab
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-07-13 18:18:14 +0200

    Update zsh plugins and Rust in update.sh

--- update.sh
+++ update.sh
@@ -7,7 +7,22 @@ COLOR_GRAY="\e[1;30m"
 COLOR_GREEN="\e[1;32m"
 COLOR_RESET="\e[0m"
 
-cargo install-update -a
+# Update zsh plugins and utilities
+
+for repo in $HOME/.zsh/*; do
+	cd "$repo"
+	git pull || true
+	cd
+done
+
+# Update Rust
+
+rustup update
+
+# Update packages
+
+cargo install-update -a &
+tldr --update &
 yarn global upgrade
 
 echo -e "\n$COLOR_GRAY"
