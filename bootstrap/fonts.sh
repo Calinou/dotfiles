8b55c33a1325ea34ebb4802b4b553cfecb066e57
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-25 20:22:21 +0200

    fonts.sh: Install Noto fonts

--- bootstrap/fonts.sh
+++ bootstrap/fonts.sh
@@ -15,3 +15,20 @@ tmp="$(mktemp)"
 curl -fsSLo "$tmp" "https://github.com/rsms/inter/releases/download/v3.0/Inter-UI-3.0.zip"
 unzip -o "$tmp" "Inter UI (TTF hinted)/*" -d "$HOME/.fonts/"
 rm "$tmp"
+
+# Install Noto fonts
+
+tmp="$(mktemp)"
+curl -fsSLo "$tmp" "https://noto-website-2.storage.googleapis.com/pkgs/NotoSans-hinted.zip"
+unzip -o "$tmp" -d "$HOME/.fonts/noto-sans/"
+rm "$tmp"
+
+tmp="$(mktemp)"
+curl -fsSLo "$tmp" "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerif-hinted.zip"
+unzip -o "$tmp" -d "$HOME/.fonts/noto-serif/"
+rm "$tmp"
+
+tmp="$(mktemp)"
+curl -fsSLo "$tmp" "https://noto-website-2.storage.googleapis.com/pkgs/NotoMono-hinted.zip"
+unzip -o "$tmp" -d "$HOME/.fonts/noto-mono/"
+rm "$tmp"
