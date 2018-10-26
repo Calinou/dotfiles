c7253805f517b688e81c014615861e6510fcbaab
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-26 11:52:55 +0200

    ruby.sh: Make the install script continue in case of failure
    The install script will complain about the rbenv directory not being
    present in the PATH and return a non-zero exit code,
    but it's actually harmless in our case since we're adding it later on.

--- bootstrap/ruby.sh
+++ bootstrap/ruby.sh
@@ -5,14 +5,12 @@ IFS=$'\n\t'
 
 # Install rbenv and Ruby if not already present
 
-if ! command -v rbenv > /dev/null 2>&1; then  
-  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+if ! command -v rbenv > /dev/null 2>&1; then
+  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash || true
   export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
 
   rbenv install 2.5.1
   rbenv global 2.5.1
-
-  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 fi
 
 # Install packages
