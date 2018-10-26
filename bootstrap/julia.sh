1662f93fe42753ffb10a9353644f43284e945423
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-26 15:21:36 +0200

    Add a Julia installation script

---
+++ bootstrap/julia.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install Julia
+
+if ! command -v julia > /dev/null 2>&1; then
+  tmp="$(mktemp)"
+  curl -fsSL "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.1-linux-x86_64.tar.gz" \
+      -o "$tmp"
+   tar xf "$tmp" -C "$HOME/.local/opt/"
+   rm "$tmp"
+   mv "$HOME/.local/opt"/julia* "$HOME/.local/opt/julia"
+
+  ln -s "$HOME/.local/opt/julia/bin/julia" "$HOME/.local/bin/julia"
+fi
