fbc2287de729841c9eacc3334692b94441fc7e30
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-30 17:20:30 +0100

    Add a DBeaver installation script

---
+++ bootstrap/dbeaver.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install DBeaver
+
+if ! command -v dbeaver > /dev/null 2>&1; then
+  tmp="$(mktemp)"
+  curl -fsSL "https://dbeaver.io/files/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz" \
+      -o "$tmp"
+  tar xf "$tmp" -C "$HOME/.local/opt/"
+  rm "$tmp"
+
+  ln -s "$HOME/.local/opt/dbeaver/dbeaver" "$HOME/.local/bin/dbeaver"
+fi
