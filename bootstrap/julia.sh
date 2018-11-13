335a2bd25ab2b7bcba30f5c929780721825c49bf
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-13 15:08:06 +0100

    julia.sh: Update to version 1.0.2

--- bootstrap/julia.sh
+++ bootstrap/julia.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 
 if ! command -v julia > /dev/null 2>&1; then
   tmp="$(mktemp)"
-  curl -fsSL "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.1-linux-x86_64.tar.gz" \
+  curl -fsSL "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.2-linux-x86_64.tar.gz" \
       -o "$tmp"
   tar xf "$tmp" -C "$HOME/.local/opt/"
   rm "$tmp"
