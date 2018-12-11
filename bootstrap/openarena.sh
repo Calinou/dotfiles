f384789332e20c17beeae4d5da3147d2cea98236
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-12-11 23:33:03 +0100

    Add an OpenArena installation script

---
+++ bootstrap/openarena.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install OpenArena
+
+tmp="$(mktemp)"
+curl -fsSL "http://download.tuxfamily.org/openarena/rel/088/openarena-0.8.8.zip" \
+    -o "$tmp"
+rm -rf \
+        "$HOME/.local/opt/openarena/" \
+        "$HOME/.local/bin/openarena" \
+        "$HOME/.local/share/applications/openarena.desktop"
+unzip "$tmp" -d "$HOME/.local/opt/"
+rm "$tmp"
+mv "$HOME/.local/opt/openarena-0.8.8/" "$HOME/.local/opt/openarena/"
+
+# Clean up unnecessary files
+rm -rf "$HOME/.local/opt/openarena"/{__MACOSX,*.app,*.dll,*.exe,*.i386}
+
+# Create a launcher script
+cat > "$HOME/.local/bin/openarena" << EOF
+#!/bin/sh
+
+cd "$HOME/.local/opt/openarena/"
+./openarena.x86_64 "\$@"
+EOF
+chmod +x "$HOME/.local/bin/openarena"
+
+# Create a desktop entry
+cat > "$HOME/.local/share/applications/openarena.desktop" << EOF
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=OpenArena
+GenericName=First-person shooter
+Comment=Fast-paced first-person shooter based on ioquake3
+Exec=$HOME/.local/bin/openarena
+Icon=openarena
+Categories=Game;
+EOF
