09998ec84d83028fbb28da1fced7e2a65025856e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-12-31 23:22:06 +0100

    crystal.sh: Update to version 0.27.0

--- bootstrap/crystal.sh
+++ bootstrap/crystal.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 # Install Crystal
 
 tmp="$(mktemp)"
-curl -fsSL "https://github.com/crystal-lang/crystal/releases/download/0.26.1/crystal-0.26.1-1-linux-x86_64.tar.gz" \
+curl -fsSL "https://github.com/crystal-lang/crystal/releases/download/0.27.0/crystal-0.27.0-1-linux-x86_64.tar.gz" \
     -o "$tmp"
 rm -rf "$HOME/.local/opt/crystal/" "$HOME/.local/bin/crystal"
 tar xf "$tmp" -C "$HOME/.local/opt/"
