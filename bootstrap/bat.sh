0d46f286107b8a4b6f7fe86206642d9b3ec07aa6
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-01-11 09:35:44 +0100

    Add a bat installation script

---
+++ bootstrap/bat.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install bat
+
+tmp_archive="$(mktemp)"
+tmp_dir="$(mktemp -d)"
+curl -fsSL "https://github.com/sharkdp/bat/releases/download/v0.9.0/bat-v0.9.0-x86_64-unknown-linux-gnu.tar.gz" \
+    -o "$tmp_archive"
+tar xf "$tmp_archive" --strip-components=1 --one-top-level="$tmp_dir/"
+mv "$tmp_dir/bat" "$HOME/.local/bin/bat"
+rm -rf "$tmp_archive" "${tmp_dir:?}/"
