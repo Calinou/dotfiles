309c1e951dd8183e7815aa9174a9a878139439db
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-02-01 17:58:09 +0100

    julia.sh: Update to version 1.1.0

--- bootstrap/julia.sh
+++ bootstrap/julia.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 # Install Julia
 
 tmp="$(mktemp)"
-curl -fsSL "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.3-linux-x86_64.tar.gz" \
+curl -fsSL "https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz" \
     -o "$tmp"
 rm -rf "$HOME/.local/opt/julia/" "$HOME/.local/bin/julia"
 tar xf "$tmp" -C "$HOME/.local/opt/"
