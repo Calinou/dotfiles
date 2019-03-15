32815f778bf4c2e919238507bcc2ea5ec5a00e64
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-03-15 11:15:50 +0100

    Add a Caprine installation script

---
+++ bootstrap/caprine.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install Caprine
+
+tmp_appimage="$(mktemp)"
+tmp_dir="$(mktemp -d)"
+curl -fsSL "https://github.com/sindresorhus/caprine/releases/download/v2.30.1/caprine-2.30.1-x86_64.AppImage" \
+    -o "$tmp_appimage"
+chmod +x "$tmp_appimage"
+(cd "$tmp_dir" && "$tmp_appimage" --appimage-extract)
+rm -rf "$HOME/.local/opt/caprine/" "$HOME/.local/bin/caprine"
+mv "$tmp_dir/squashfs-root" "$HOME/.local/opt/caprine/"
+rm -rf "$tmp_appimage" "$tmp_dir"
+
+ln -s "$HOME/.local/opt/caprine/caprine" "$HOME/.local/bin/caprine"
+convert -resize 256x256 \
+    "$HOME/.local/opt/caprine/usr/share/icons/hicolor/0x0/apps/caprine.png" \
+    "$HOME/.local/share/icons/hicolor/256x256/apps/caprine.png"
+
+cat > "$HOME/.local/share/applications/caprine.desktop" << EOF
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=Caprine
+Comment=Facebook Messenger desktop app
+Exec=$HOME/.local/bin/caprine
+Icon=caprine
+Categories=Internet;
+EOF
