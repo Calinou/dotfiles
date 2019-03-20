38cdeecb98f6683f1a2ea1779af3d8d82d9ed98e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-03-20 13:21:45 +0100

    Add a Pulverize installation script

---
+++ bootstrap/pulverize.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install Pulverize
+
+tmp="$(mktemp)"
+curl -fsSL "https://github.com/sciactive/pulverize/archive/master.tar.gz" \
+    -o "$tmp"
+rm -rf "$HOME/.local/{bin,opt}/pulverize/"
+tar xf "$tmp" --strip-components=1 --one-top-level="$HOME/.local/opt/pulverize/"
+rm "$tmp"
+
+cat > "$HOME/.local/bin/pulverize" << 'EOF'
+#!/bin/sh
+
+cd "$HOME/.local/opt/pulverize/"
+./pulverize.php "$@"
+EOF
+chmod +x "$HOME/.local/bin/pulverize"
