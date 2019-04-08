a2b42e6e97d5c2ec0b0fc1e9743d42a729731057
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-04-08 18:41:45 +0200

    completion.sh: Use docker-compose autocompletion from the master branch

--- bootstrap/completion.sh
+++ bootstrap/completion.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 mkdir -p "$HOME/.zsh/completion/"
-curl -fsSL "https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/zsh/_docker-compose" \
+curl -fsSL "https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose" \
     -o "$HOME/.zsh/completion/_docker-compose"
