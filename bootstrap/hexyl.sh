3c4cc75ebd77f20b38216d0391e6bbef1f3cd63c
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-04-08 17:28:52 +0200

    hexyl: Update to version 0.4.0

--- bootstrap/hexyl.sh
+++ bootstrap/hexyl.sh
@@ -5,8 +5,10 @@ IFS=$'\n\t'
 
 # Install hexyl
 
+VERSION="0.4.0"
+
 tmp="$(mktemp)"
-curl -fsSL "https://github.com/sharkdp/hexyl/releases/download/v0.3.1/hexyl-v0.3.1-x86_64-unknown-linux-musl.tar.gz" \
+curl -fsSL "https://github.com/sharkdp/hexyl/releases/download/v$VERSION/hexyl-v$VERSION-x86_64-unknown-linux-musl.tar.gz" \
     -o "$tmp"
-tar xf "$tmp" "hexyl-v0.3.1-x86_64-unknown-linux-musl/hexyl" --strip-components=1 --one-top-level="$HOME/.local/bin/"
+tar xf "$tmp" "hexyl-v$VERSION-x86_64-unknown-linux-musl/hexyl" --strip-components=1 --one-top-level="$HOME/.local/bin/"
 rm "$tmp"
