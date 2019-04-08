2a07fe0142bea9e339760eed8d9e5d8fdaa58312
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-04-08 18:43:14 +0200

    zsh.sh: Make paths to directories explicit

--- bootstrap/zsh.sh
+++ bootstrap/zsh.sh
@@ -7,13 +7,13 @@ IFS=$'\n\t'
 
 git clone --depth=1 \
     "https://github.com/basherpm/basher.git" \
-    "$HOME/.basher" || true
+    "$HOME/.basher/" || true
 
 # Install Prezto
 
 git clone --depth=1 --recursive \
     "https://github.com/sorin-ionescu/prezto.git" \
-    "${ZDOTDIR:-$HOME}/.zprezto" || true
+    "${ZDOTDIR:-$HOME}/.zprezto/" || true
 
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*; do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.$(basename "$rcfile")" || true
@@ -23,19 +23,19 @@ done
 
 git clone --depth=1 \
     "https://github.com/zsh-users/zsh-autosuggestions.git" \
-    "$HOME/.zsh/zsh-autosuggestions" || true
+    "$HOME/.zsh/zsh-autosuggestions/" || true
 
 # Install Code::Stats zsh plugin
 
 git clone --depth=1 \
     "https://gitlab.com/code-stats/code-stats-zsh.git" \
-    "$HOME/.zsh/code-stats-zsh" || true
+    "$HOME/.zsh/code-stats-zsh/" || true
 
 # Install goto
 
 git clone --depth=1 \
     "https://github.com/iridakos/goto.git" \
-    "$HOME/.zsh/goto" || true
+    "$HOME/.zsh/goto/" || true
 
 # Install diff-so-fancy
 
