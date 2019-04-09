0e8d481e8ba03a727b05fd21f1b786046cf99fe5
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-04-09 22:09:29 +0200

    fonts.sh: Use version variables

--- bootstrap/fonts.sh
+++ bootstrap/fonts.sh
@@ -11,24 +11,30 @@ git clone --depth 1 \
 
 # Install Hack font
 
+HACK_VERSION="3.003"
+
 tmp="$(mktemp)"
-curl -fsSL "https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip" \
+curl -fsSL "https://github.com/source-foundry/Hack/releases/download/v$HACK_VERSION/Hack-v$HACK_VERSION-ttf.zip" \
     -o "$tmp"
 unzip -jo "$tmp" "ttf/*" -d "$HOME/.fonts/"
 rm "$tmp"
 
 # Install IBM Plex
 
+IBM_PLEX_VERSION="1.4.1"
+
 tmp="$(mktemp)"
-curl -fsSL "https://github.com/IBM/plex/releases/download/v1.2.3/TrueType.zip" \
+curl -fsSL "https://github.com/IBM/plex/releases/download/v$IBM_PLEX_VERSION/TrueType.zip" \
     -o "$tmp"
 unzip -o "$tmp" "TrueType/*" -d "$HOME/.fonts/"
 rm "$tmp"
 
 # Install Inter
 
+INTER_VERSION="3.5"
+
 tmp="$(mktemp)"
-curl -fsSL "https://github.com/rsms/inter/releases/download/v3.3/Inter-3.3.zip" \
+curl -fsSL "https://github.com/rsms/inter/releases/download/v$INTER_VERSION/Inter-$INTER_VERSION.zip" \
     -o "$tmp"
 unzip -o "$tmp" "Inter (TTF hinted)/*" -d "$HOME/.fonts/"
 rm "$tmp"
