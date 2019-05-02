e19ee8e5a7168444887a432f6562f1018b1325c6
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-05-02 12:25:24 +0200

    Add a SuperTuxKart installation script

---
+++ bootstrap/supertuxkart.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install SuperTuxKart
+
+tmp="$(mktemp)"
+curl -fsSL "https://downloads.sourceforge.net/project/supertuxkart/SuperTuxKart/1.0/supertuxkart-1.0-linux.tar.xz" \
+    -o "$tmp"
+rm -rf \
+    "$HOME"/.local/{bin,opt}/supertuxkart \
+    "$HOME/.local/share/applications/supertuxkart.desktop"
+tar xf "$tmp" --strip-components=1 --one-top-level="$HOME/.local/opt/supertuxkart/"
+rm "$tmp"
+
+cat > "$HOME/.local/bin/supertuxkart" << 'EOF'
+#!/bin/sh
+
+cd "$HOME/.local/opt/supertuxkart/"
+./run_game.sh "$@"
+EOF
+chmod +x "$HOME/.local/bin/supertuxkart"
+
+cat > "$HOME/.local/share/applications/supertuxkart.desktop" << EOF
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=SuperTuxKart
+GenericName=Racing game
+Comment=Arcade racing game
+Exec=$HOME/.local/bin/supertuxkart
+Icon=supertuxkart
+Categories=Game;
+EOF
