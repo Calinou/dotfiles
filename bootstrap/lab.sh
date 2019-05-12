d272ad4cb4c8cedab68d20a3cdbcdf41bca68439
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-05-12 14:42:22 +0200

    Add a lab installation script

---
+++ bootstrap/lab.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install lab
+
+VERSION="0.15.3"
+
+tmp="$(mktemp)"
+curl -fsSL "https://github.com/zaquestion/lab/releases/download/v$VERSION/lab_${VERSION}_linux_amd64.tar.gz" \
+    -o "$tmp"
+tar xf "$tmp" "lab" --one-top-level="$HOME/.local/bin/"
+rm "$tmp"
