98f4dfd14508b998836b06883902b0d85788b950
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-06-13 17:46:08 +0200

    gomplate.sh: Update to version 3.5.0

--- bootstrap/gomplate.sh
+++ bootstrap/gomplate.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 
 # Install gomplate
 
-VERSION="3.3.1"
+VERSION="3.5.0"
 
 curl -fsSL "https://github.com/hairyhenderson/gomplate/releases/download/v$VERSION/gomplate_linux-amd64" \
     -o "$HOME/.local/bin/gomplate"
