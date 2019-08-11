ec06bbdddcedc81c50cd19adbcdfc75413c990dc
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-08-11 21:57:01 +0200

    fonts.sh: Update Inter to version 3.9

--- bootstrap/fonts.sh
+++ bootstrap/fonts.sh
@@ -39,7 +39,7 @@ rm "$tmp"
 
 # Install Inter
 
-INTER_VERSION="3.8"
+INTER_VERSION="3.9"
 
 tmp="$(mktemp)"
 curl -fsSL "https://github.com/rsms/inter/releases/download/v$INTER_VERSION/Inter-$INTER_VERSION.zip" \
