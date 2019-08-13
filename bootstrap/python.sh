e4120805f762e1e175e966e8bec4f940177d675f
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-08-13 23:38:13 +0200

    python.sh: Update to version 2.7.16

--- bootstrap/python.sh
+++ bootstrap/python.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 # Install pyenv and Python if not already present
 
 PYTHON3_VERSION="3.7.4"
-PYTHON2_VERSION="2.7.15"
+PYTHON2_VERSION="2.7.16"
 
 if ! command -v pyenv > /dev/null 2>&1; then
   tmp="$(mktemp)"
