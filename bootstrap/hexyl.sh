a36bb5d3d66823cbfab6d5c2b502f2ebd893136e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-12-30 23:58:50 +0100

    hexyl.sh: Update to version 0.6.0

--- bootstrap/hexyl.sh
+++ bootstrap/hexyl.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 
 # Install hexyl
 
-VERSION="0.5.1"
+VERSION="0.6.0"
 
 tmp="$(mktemp)"
 curl -fsSL "https://github.com/sharkdp/hexyl/releases/download/v$VERSION/hexyl-v$VERSION-x86_64-unknown-linux-musl.tar.gz" \
