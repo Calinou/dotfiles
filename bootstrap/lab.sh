b318bf361ed1c7295a7c6949cdd4b1e396d61556
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-12-31 00:00:32 +0100

    lab.sh: Update to version 0.17.2

--- bootstrap/lab.sh
+++ bootstrap/lab.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 
 # Install lab
 
-VERSION="0.16.0"
+VERSION="0.17.2"
 
 tmp="$(mktemp)"
 curl -fsSL "https://github.com/zaquestion/lab/releases/download/v$VERSION/lab_${VERSION}_linux_amd64.tar.gz" \
